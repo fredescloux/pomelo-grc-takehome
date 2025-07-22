a57e0fb528de76a14bc2c6c58d02a24a98814a8d
Frederic Descloux <fredericdescloux@Freds-MacBook-Air.local>
2025-07-21 22:24:37 -0400

    Implemented bonus: fixed key issue in code

--- frontend/src/components/InputForm.jsx
+++ frontend/src/components/InputForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import dayjs from 'dayjs';
 import { importSPKI, jwtVerify } from 'jose';
 
-const publicKey = `-----BEGIN PUBLIC KEY-----MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAE5HtLv0yI7V1H/VB2DjxMguuFy6rbQXGxq2HqReG1bT0NzrOqWXU+2+H4cZCPc6gT1s3l3ibUOBDmv60DkzGzexEZMAwPHDM0D15I6lEOQkaDYqqYwPMdl3kp8aF4YNUX-----END PUBLIC KEY-----`;
+const publicKey = `-----BEGIN PUBLIC KEY-----MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEMU1JFVEO9FkVr0r041GpAWzKvQi1TBYmarJj3+aNeC2aK9GT7Hct1OJGWQGbUkNWTeUr+Ui09PjBit+AMYuHgA==-----END PUBLIC KEY-----`;
 
 
 const InputForm = ({ onSubmit }) => {
